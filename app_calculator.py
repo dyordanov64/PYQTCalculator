53ca66953f35e2805bf8ebc56eb032584915706b
dyordanov64 <d.yordanov64@abv.bg>
2023-09-20 19:21:52 +0300

    error

---
+++ app_calculator.py
@@ -0,0 +1,15 @@
+# 1. import needed QtWidgets classes
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtCore, QtGui, QtWidgets
+from UI.calculator import Ui_MyMainWindow
+
+# 2. the main app instance for our application.
+app = QApplication([])
+
+# 3. Create Qt widget, which will be our main window.
+window = QWidget()
+Main = Ui_MyMainWindow()
+Main.setupUi(window)
+
+# 4. show the window
+window.show
