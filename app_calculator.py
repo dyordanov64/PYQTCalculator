e6a935a82e36da1bd0d618c3cfa311bbc314ae4f
dyordanov64 <d.yordanov64@abv.bg>
2023-09-25 19:19:05 +0300

    lab41

--- app_calculator.py
+++ app_calculator.py
@@ -1,5 +1,5 @@
 # 1. import needed QtWidgets classes
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt6 import QtCore, QtGui, QtWidgets
 from UI.calculator import Ui_MyMainWindow
 
@@ -7,9 +7,10 @@ from UI.calculator import Ui_MyMainWindow
 app = QApplication([])
 
 # 3. Create Qt widget, which will be our main window.
-window = QWidget()
+window = QMainWindow()
 Main = Ui_MyMainWindow()
 Main.setupUi(window)
 
 # 4. show the window
-window.show
+window.show()
+app.exec()
